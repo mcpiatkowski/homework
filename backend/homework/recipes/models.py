09b33e702d919b418b61c6a4d1713679a59e7072
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-29 11:34:30 +0200

    description added

--- backend/homework/recipes/models.py
+++ backend/homework/recipes/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=70, default='', blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,6 +11,7 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=70, default='', blank=True)
+    description = models.TextField(null=True, blank=True)
     ingredients = models.ManyToManyField(Ingredient)
 
     def __str__(self):
